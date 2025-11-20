8a170b3650c214635a5e537abe582e9bc3e26788
AidenJoseRHS <aidenjoserhs@gmail.com>
2025-11-20 08:41:22 +0000

    Added Multiplier System

--- Widgets/widget.js
+++ Widgets/widget.js
@@ -1,15 +1,25 @@
-let muscleEl = document.getElementById("muscle-el");
-let muscle = 0;
-
-let scream = document.getElementById("scream"); 
+let muscleEl = document.getElementById("muscle-el")
+let muscle = 0
+let muscleGain = 1
+let scream = document.getElementById("scream")
+let boom = document.getElementById("boom")
+let baconz = document.getElementById("baconz")
+let guestz = document.getElementById("guestz")
 
 function clickers() {
-    muscle += 1;
+    muscle += muscleGain;
     muscleEl.textContent = muscle;
 
     scream.play();
     scream.currentTime = 0.2;
 
-    
 }
 clickers()
+
+function baconUpgrade() {
+muscleGain += 1
+muscleEl += 1
+    boom.play();
+    boom.currentTime = 0.3;
+}
+
