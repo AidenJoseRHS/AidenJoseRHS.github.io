f621187a48cd9bf9461a443fa37523968bf6e937
AidenJoseRHS <aidenjoserhs@gmail.com>
2025-11-20 17:56:39 +0000

    Made Upgrades to the widget, and more sounds

--- Widgets/widget.js
+++ Widgets/widget.js
@@ -1,10 +1,12 @@
-let muscleEl = document.getElementById("muscle-el")
-let muscle = 0
-let muscleGain = 1
-let scream = document.getElementById("scream")
-let boom = document.getElementById("boom")
-let baconz = document.getElementById("baconz")
-let guestz = document.getElementById("guestz")
+let muscleEl = document.getElementById("muscle-el");
+let gainEl = document.getElementById("gain-el");
+let muscle = 0;
+let muscleGain = 1;
+let scream = document.getElementById("scream");
+let boom = document.getElementById("boom");
+let baconz = document.getElementById("baconz");
+let guestz = document.getElementById("guestz");
+let nutton = document.getElementById("nutton");
 
 function clickers() {
     muscle += muscleGain;
@@ -16,9 +18,21 @@ function clickers() {
 }
 clickers()
 
+function multiplier() {
+   gainEl.textcontent = muscleGain;
+}
+
 function baconUpgrade() {
 muscleGain += 1
     boom.play();
     boom.currentTime = 0.3;
+}
+
+function guestUpgrade() {
+    muscle += 5;
+    angy.play();
+    angy.currentTime = 0.4;
+    setTimeout(guestUpgrade, 5000);
+    muscleEl.textContent = muscle;
 }
 
